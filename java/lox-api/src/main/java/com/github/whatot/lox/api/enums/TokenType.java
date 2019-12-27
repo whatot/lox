715d540bbcb39ff8a2d5bec5b2e7061fdc2087bf
whatot <whatot2@gmail.com>
2019-12-27 20:49:09 +0800

    add init pom.xml

--- java/lox-api/src/main/java/com/github/whatot/lox/api/enums/TokenType.java
+++
@@ -1,8 +0,0 @@
-package com.github.whatot.lox.api.enums;
-
-/**
- * @author shenlou.huang
- * @date 2019/12/27
- */
-public enum TokenType {
-}
--- java/lox-api/src/main/java/com/github/whatot/lox/api/model/Token.java
+++
@@ -1,8 +0,0 @@
-package com.github.whatot.lox.api.model;
-
-/**
- * @author shenlou.huang
- * @date 2019/12/27
- */
-public class Token {
-}
